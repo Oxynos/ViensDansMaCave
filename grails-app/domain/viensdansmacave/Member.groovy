053feec3555d1bd455b4bbb04db03d126550c5d2
lpierillas <lucie.pierillas@gmail.com>
2015-10-02 08:53:09 +0200

    tests unitaires pour la classe Member

--- grails-app/domain/viensdansmacave/Member.groovy
+++ grails-app/domain/viensdansmacave/Member.groovy
@@ -8,6 +8,10 @@ class Member implements Serializable {
 
 	String username
 	String password
+	String email
+	Integer age
+	String country
+	String city
 	boolean enabled = true
 	boolean accountExpired
 	boolean accountLocked
@@ -57,6 +61,10 @@ class Member implements Serializable {
 	static constraints = {
 		username blank: false, unique: true
 		password blank: false
+		email email:true, nullable: true
+		age nullable: true
+		country nullable: true
+		city nullable: true
 	}
 
 	static mapping = {
